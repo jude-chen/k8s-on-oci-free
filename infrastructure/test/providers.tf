ddc1741d04702e2c33d824e02a3a1e305fcb197e
Jude Chen <jchen@brighthealthplan.com>
2022-05-12 12:15:00 -0500

    added the provider block.

--- infrastructure/test/providers.tf
+++ infrastructure/test/providers.tf
@@ -1,8 +1,10 @@
 terraform {
-    required_providers {
-        oci = {
-            source = "oracle/oci"
-            version = "~> 4.0"
-        }
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 4.0"
     }
+  }
 }
+
+provider "oci" {}
